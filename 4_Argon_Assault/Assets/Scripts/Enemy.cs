a3bd784694f00b2f1c50c2de3c83cb79281c3f6e
Agilsaav <agilsaav@gmail.com>
2019-12-06 14:14:15 +0100

    proj update

---
+++ 4_Argon_Assault/Assets/Scripts/Enemy.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] int scorePerHit = 12;
+    [SerializeField] int hits = 5;
+    [SerializeField] GameObject deathFX;
+
+    ScoreBoard scoreBoard;
+    // Start is called before the first frame update
+    void Start()
+    {
+        AddNonTriggerBoxCollider();
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+    }
+
+    private void AddNonTriggerBoxCollider()
+    {
+        Collider boxCollider = gameObject.AddComponent<BoxCollider>();
+        boxCollider.isTrigger = false;
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        ProcessHit();
+        if (hits <= 0)
+            KillEnemy();
+    }
+
+    private void ProcessHit()
+    {
+        scoreBoard.ScoreHit(scorePerHit);
+        hits--;
+    }
+
+    private void KillEnemy()
+    {
+        Instantiate(deathFX, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
---
+++ 4_Argon_Assault/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("General")]
+    [Tooltip("In ms^-1")] [SerializeField] float controllSpeed = 10.0f;
+    [Tooltip("In ms^-1")] [SerializeField] float xRange = 8.5f;
+    [Tooltip("In ms^-1")] [SerializeField] float yRange = 4.5f;
+    [SerializeField] GameObject[] guns;
+
+    [Header("Screen-position Based")]
+    [SerializeField] float PositionPitchFactor = -5.0f;
+    [SerializeField] float PositionYawFactor = 7.0f;
+
+    [Header("Control-throw Based ")]
+    [SerializeField] float controlPitchFactor = -20.0f;
+    [SerializeField] float controlRollFactor = -20.0f;
+
+    float xThrow, yThrow;
+    bool isControlEnabled = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isControlEnabled)
+        {
+            ProcessTranslation();
+            ProcessRotation();
+            ProcessFiring();
+        }
+    }
+
+    private void ProcessTranslation()
+    {
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+
+        float xOffset = xThrow * controllSpeed * Time.deltaTime;
+        float yOffset = yThrow * controllSpeed * Time.deltaTime;
+
+        float XPos = Mathf.Clamp(transform.localPosition.x + xOffset, -xRange, xRange);
+        float YPos = Mathf.Clamp(transform.localPosition.y + yOffset, -yRange, yRange);
+
+        transform.localPosition = new Vector3(XPos, YPos, transform.localPosition.z);
+    }
+
+    private void ProcessRotation()
+    {
+        float pitch = transform.localPosition.y * PositionPitchFactor + yThrow * controlPitchFactor;
+        float yaw = transform.localPosition.x * PositionYawFactor;
+        float roll = xThrow * controlRollFactor;
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+
+    void OnPlayerDeath()
+    {
+        isControlEnabled = false;
+    }
+
+    private void ProcessFiring()
+    {
+        if (CrossPlatformInputManager.GetButton("Fire"))
+            SetGunsActive(true);
+        else
+            SetGunsActive(false);
+    }
+
+    private void SetGunsActive(bool isActive)
+    {
+        foreach (GameObject gun in guns)
+        {
+            var emissionModule = gun.GetComponent<ParticleSystem>().emission;
+            emissionModule.enabled = isActive;
+        }
+    }
+
+}
